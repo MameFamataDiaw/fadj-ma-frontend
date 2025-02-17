cca24b3c3f117d5a034f88b0d563280f060e8fd8
Fams-tech <diawmamefama@gmail.com>
2025-02-17 13:18:29 +0000

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Dashboard from "./dashboard/dashboard"
 import InventoryPage from "@/app/component/Medicament/InventoryPage";
 import Topbar from "@/app/component/Bar/Topbar";
 import Sidebar from "@/app/component/Bar/Sidebar";
-import { useEffect } from "react";
+import {useEffect} from "react";
 
 export default function Home() {
     const router = useRouter();
@@ -17,14 +17,22 @@ export default function Home() {
     }, [router]);
 
     return (
-        <div>
-            <Topbar />
-            <div style={{ display: 'flex' }}>
-                <Sidebar />
-                <div>
-                    <Dashboard />
-                </div>
-            </div>
-        </div>
-    );
+        // useEffect(() => {
+        //     const token = localStorage.getItem("authToken");
+        //     if (!token) {
+        //         router.push("/login"); // Redirige si l'utilisateur n'est pas authentifié
+        //     }
+        // }, []);
+
+    <div>
+          <Topbar />
+          <div style={{ display: 'flex'}}>
+              <Sidebar />
+              <div style={{ flex: 1, padding: 0 }}>
+                  <Dashboard />
+                  {/*<InventoryPage />*/}
+              </div>
+          </div>
+      </div>
+  );
 }
